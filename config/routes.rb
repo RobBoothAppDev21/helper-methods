ab4814e4c1b2be83cd2a779497392f0066fd3739
Rob Young <rhyoung214@gmail.com>
2022-04-10 14:50:22 -0500

    Updated routes to use resources :movies

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,21 @@ Rails.application.routes.draw do
   root 'movies#index'
 
   # Routes for the Movie resource:
-
+  resources :movies
   # CREATE
-  post '/movies' => 'movies#create', as: :movies # movies_url and movies_path
-  get 'movies/new' =>  'movies#new', as: :new_movie # new_movie_url and new_movie_path
+  # post '/movies' => 'movies#create', as: :movies # movies_url and movies_path
+  # get 'movies/new' => 'movies#new', as: :new_movie # new_movie_url and new_movie_path
 
-  # READ
-  get '/movies' => 'movies#index'
-  get '/movies/:id' => 'movies#show', as: :movie # movie_path() -> expects argmument to populate :id
+  # # READ
+  # get '/movies' => 'movies#index'
+  # get '/movies/:id' => 'movies#show', as: :movie # movie_path() -> expects argmument to populate :id
 
-  # UPDATE
-  patch '/movies/:id' => 'movies#update'
-  get '/movies/:id/edit' => 'movies#edit', as: :edit_movie # edit_movie_path()
+  # # UPDATE
+  # patch '/movies/:id' => 'movies#update'
+  # get '/movies/:id/edit' => 'movies#edit', as: :edit_movie # edit_movie_path()
 
-  # DELETE
-  delete '/movies/:id' => 'movies#destroy'
+  # # DELETE
+  # delete '/movies/:id' => 'movies#destroy'
 
   #------------------------------
 end
